534ac5424b429467c92377cdd4b293b9fb04d6c1
Naomi Lui-HIng <naomiluihing@cmail.carleton.ca>
2018-10-01 12:59:21 -0400

    Changes to methodC()
    help

--- Hello.java
+++ Hello.java
@@ -17,5 +17,7 @@ public class Hello {
             System.out.println("hello\n");
 	}
 
-	public void methodC(void) { }
+	public void methodC(void) { 
+            System.out.println("Help\n");
+        }
 }
